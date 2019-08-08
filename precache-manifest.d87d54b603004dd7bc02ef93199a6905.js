76805a2affd7e4cfd03987031e7ac730a36fcbc8
Otim Fredrick <fredrickot@gmail.com>
2019-08-08 12:47:53 +0300

    Updates

---
+++ precache-manifest.d87d54b603004dd7bc02ef93199a6905.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45aabbd040ad807e59938512d9732ad3",
+    "url": "/react-hooks/index.html"
+  },
+  {
+    "revision": "59be91fe7e3ec45ef406",
+    "url": "/react-hooks/static/css/main.60ef80d5.chunk.css"
+  },
+  {
+    "revision": "609637d649fbdf27206c",
+    "url": "/react-hooks/static/js/2.74462cb6.chunk.js"
+  },
+  {
+    "revision": "59be91fe7e3ec45ef406",
+    "url": "/react-hooks/static/js/main.9981d74e.chunk.js"
+  },
+  {
+    "revision": "bb9cb6b5f5410997d1d8",
+    "url": "/react-hooks/static/js/runtime~main.d8882185.js"
+  }
+]);
